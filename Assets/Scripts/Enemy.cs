1bc40c0824e21732013f0fc2052dbe762c1e79f2
N0edel <jfburgerton@gmail.com>
2026-05-09 14:10:15 -0400

    david

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,25 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Structs;
 
-private void OnTriggerEnter2D(Collider2D collision)
+public class Enemy : MonoBehaviour
 {
-    string LightWindtag = null;
-    // If it touches the bullet, it updates 
-    if (collision.gameObject.tag == LightWindtag)
+    //==================================================================================================================
+    // Variables 
+    //==================================================================================================================
+
+    //Movement Controls 
+    public Rigidbody2D rigidbody2D; //The rigidbody that will move the bullet 
+    public float minSpeed = 1f;           //Speed at which the bullet moves 
+    public float maxSpeed = 4f;           //Speed at which the bullet moves 
+
+    //Flag and Timer 
+    public float deathTime = 100f;   //How long before the bullet dies 
+    public bool playerBullet = true; //Is the bullet used by player or enemy 
+
+    // Tags and Names 
+    private string boundsTag = "Bounds";
+    private string bulletTag = "Bullet";
+    private string gameControllerComponent = "GameController";
+
+    // Component 
+    public GameController _hw2GameController;
+
+    //==================================================================================================================
+    // Base Method  
+    //==================================================================================================================
+
+    //Checks who is shooting the bullet and set up the bullet settings 
+    private void Start()
     {
-        //Updates the Score 
-        _hw2GameController.UpdateScore();
-        //Destorys the bullet
-        Destroy(collision.gameObject);
-        //Destorys the enemy 
-        Destroy(gameObject);
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
-    // If the enemy touches a bound it gets destored 
-    else if (collision.gameObject.tag == boundsTag)
+
+    public void SetSpeed(Vector3 newSpeed)
     {
-        Destroy(gameObject);
+        rigidbody2D.velocity = newSpeed * Random.Range(minSpeed, maxSpeed);
+    }
+
+    //==================================================================================================================
+    // Bullet Set Up  
+    //==================================================================================================================
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Exit")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            GetComponent<Collider2D>().enabled = false; 
+
+        }
     }
 }
-}
+
+
+
